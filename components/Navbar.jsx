f79293eeb0cd022c3e6582a5bebd7714e75e4036
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-08-22 13:51:31 +0530

    Navbar built

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,27 @@
-import { HStack } from '@chakra-ui/react'
-import React from 'react'
-import { Link as ReactRouterLink } from 'react-router-dom'
-import { Link as ChakraLink } from '@chakra-ui/react'
+import { Flex, HStack, Spacer } from "@chakra-ui/react";
+import React from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
 const Navbar = () => {
   return (
-    <HStack>
-        <ChakraLink  as={ReactRouterLink} to={"/book/create"}>Create Book</ChakraLink>
-        <ChakraLink  as={ReactRouterLink} to={"/user/register"}>Register</ChakraLink>
-        <ChakraLink  as={ReactRouterLink} to={"/user/login"}>Login</ChakraLink>
-    </HStack>
-      
-   
-  )
-}
+    <Flex bgColor={"pink"} px={"60px"} py={3} fontSize={34} fontWeight={500}  >
+      <ChakraLink as={ReactRouterLink} to={"/"} _hover={{color:"teal"}}>
+        Ayush Dwivedi
+      </ChakraLink>
+      <Spacer />
+      <ChakraLink as={ReactRouterLink} to={"/book/create"} _hover={{color:"teal"}}>
+        Create Book
+      </ChakraLink>
+      <Spacer />
+      <ChakraLink as={ReactRouterLink} to={"/user/register"} _hover={{color:"teal"}}>
+        Register
+      </ChakraLink>
+      <Spacer />
+      <ChakraLink as={ReactRouterLink} to={"/user/login"} _hover={{color:"teal"}}>
+        Login
+      </ChakraLink>
+    </Flex>
+  );
+};
 
-export default Navbar
+export default Navbar;
